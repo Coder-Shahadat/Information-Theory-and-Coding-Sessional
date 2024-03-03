0a7d982059cbbdf92e4adbad19694200db1a3e2e
MD. Shahadat Hossain Bhuian <1337.Shahadat@gmail.com>
2024-03-03 14:47:51 +0600

    Huffman Coding

--- lab1.py
+++ lab1.py
@@ -1,33 +1,37 @@
 import heapq
-from collections import defaultdict, Counter
+from collections import Counter
 
 
-def calculate_frequency(my_text):
-    my_text = my_text.upper().replace(' ', '')
-    frequency = dict(Counter(my_text))
-    return frequency
+def Probabilities(message):
+    message = message.upper().replace(' ', '')
+    freq = Counter(message)
+    P = [fre / len(message) for fre in freq.values()]
+    return P, freq
 
 
 def build_heap(freq):
-    heap = [[weight, [char, ""]] for char, weight in freq.items()]
+    heap = [[weight / sum(freq.values()), [char, '']] for char, weight in freq.items()]
     heapq.heapify(heap)
     return heap
 
 
 def build_tree(heap):
     while len(heap) > 1:
-        lo = heapq.heappop(heap)
-        hi = heapq.heappop(heap)
-        for pair in lo[1:]:
+        left = heapq.heappop(heap)
+        right = heapq.heappop(heap)
+        for pair in left[1:]:
             pair[1] = '0' + pair[1]
-        for pair in hi[1:]:
+        for pair in right[1:]:
             pair[1] = '1' + pair[1]
-        heapq.heappush(heap, [lo[0] + hi[0]] + lo[1:] + hi[1:])
+        heapq.heappush(heap, [left[0] + right[0]] + left[1:] + right[1:])
     return heap[0]
 
 
-freq = calculate_frequency("aaabbbbbccccccddddee")
+message = 'aaabbbbbccccccddddee'
+P, freq = Probabilities(message)
+print('Probabilities: ', P)
 heap = build_heap(freq)
 tree = build_tree(heap)
+print('Tree:', tree)
 for pair in tree[1:]:
     print(pair[0], '->', pair[1])
